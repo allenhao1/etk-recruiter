52eeb8465d2fd7e3ac290af62e68244a09b72c9f
Allen Hao <allen.hao@wustl.edu>
2017-02-06 23:14:07 -0600

    Phantom having problems opening page

--- scrape.js
+++ scrape.js
@@ -2,7 +2,10 @@
 var page = require('webpage').create();
 var login = 'http://www.whenisgood.net/Login';
 var home = 'http://whenisgood.net/35rxcyk/results/jger2xs';
-var conf = require('./conf.json')
+var fs = require("fs");
+var conf = JSON.parse(fs.read("conf.json"));
+console.log(conf['email'])
+console.log(conf['password'])
 
 page.open(login, function (status) {
     if (status !== 'success') {
@@ -10,22 +13,23 @@ page.open(login, function (status) {
         console.log(status);
         phantom.exit(1);
     } else {
-        page.evaluate(function(){
-            $("input[name=loginEmail]").val(conf['email']) ;
-            $("input[name=password]").val(conf['password']) ;
-            $("input[type=submit]").click() ;
-        });
+        page.evaluate(function(conf){
+          $("input[name=loginEmail]").val(conf['email']) ;
+          $("input[name=loginPassword]").val(conf['password']) ;
+          $("input[type=submit]").click() ;
+        }, conf);
         setTimeout(function(){
+          page.render('page.png');
             page.open(home, function(status){
                 if (status !== "success") {
                     console.log('fail2');
                     phantom.exit(1);
                     return;
                 }
-                console.log(page.url);
-                page.render('page.png');
-                console.log('finished!');
-                phantom.exit();
+              console.log(page.url);
+              page.render('page.png');
+              console.log('finished!');
+              phantom.exit();
             });
         }, 500);
     }
