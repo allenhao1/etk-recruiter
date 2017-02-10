b919e381dac6fcc868743afa722826dbfc704d4a
Allen Hao <allen.hao@wustl.edu>
2017-02-09 23:17:52 -0600

    Spooky script can pull table with times for ID's, but running into an error trying to load gif files from td's

--- spooky.js
+++ spooky.js
@@ -1,47 +1,49 @@
 var fs = require('fs');
 var conf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
-
+var $ = require('jquery');
 try {
-    var Spooky = require('spooky');
+  var Spooky = require('spooky');
 } catch (e) {
-    var Spooky = require('../lib/spooky');
+  var Spooky = require('../lib/spooky');
 }
 
 var spooky = new Spooky({
-        child: {
-            transport: 'http'
-        },
-        casper: {
-            logLevel: 'debug',
-            verbose: true
-        }
-    }, function (err) {
-        if (err) {
-            e = new Error('Failed to initialize SpookyJS');
-            e.details = err;
-            throw e;
-        }
-        var code = '2xdzp5k';
-        var id = 'igjq25h';
-        var site = 'http://whenisgood.net/' + id + '/results/' + code;
-        spooky.start(site, function() {
-          this.emit('hello', this.getTitle());
-        });
-        spooky.then(function () {
-          html  = this.getElementInfo('body > script:nth-child(15)').html;
-          this.emit('eval', html);
+  child: {
+    transport: 'http'
+  },
+  casper: {
+    logLevel: 'debug',
+    verbose: true
+  }
+}, function (err) {
+  if (err) {
+    e = new Error('Failed to initialize SpookyJS');
+    e.details = err;
+    throw e;
+  }
+  var code = '2xdzp5k';
+  var id = 'igjq25h';
+  var site = 'http://whenisgood.net/' + id + '/results/' + code;
+  spooky.start(site, function() {
+    this.emit('hello', this.getTitle());
+  });
+  spooky.then(function () {
+    table = this.getElementInfo('#grid').html;
+    script  = this.getElementInfo('body > script:nth-child(15)').html;
+    this.emit('hello', "chec");
+    this.emit('eval', [table, script]);
 
-        });
-        spooky.run();
+  });
+  spooky.run();
 
-    });
+});
 
 spooky.on('error', function (e, stack) {
-      console.error(e);
+  console.error(e);
 
-    if (stack) {
-        console.log(stack);
-    }
+  if (stack) {
+    console.log(stack);
+  }
 });
 
 /*
@@ -49,30 +51,62 @@ spooky.on('error', function (e, stack) {
 // There are a lot.
 // He has opinions.
 spooky.on('console', function (line) {
-    console.log(line);
+console.log(line);
 });
 */
-spooky.on('eval', function(text){
-  var unwanted  = /disableSelection.+;|paintCanDos\(\);|mySlider\(\);/g // Contains missing functions and space
-  console.log(text)
-  text = text.replace(unwanted, " ")
-  console.log(text)
-  console.log(typeof(text))
+function getTableData(table) {
+  var timeRef = {};
   debugger
-  eval(text)
-  for (var i in respondents) {
-    console.log(respondents[i]);
-  }
-  console.log("test")
+  $(table).each(function () {
+    $('td', this).each(function () {
+      var text = $(this).text().trim();
+      var id = $(this).attr('id');
+      if (text != "" && id != undefined && id != false ) {
+          console.log(id);
+          console.log(text);
+          timeRef[id] = text;
+      }
+    });
+  });
+  console.log(timeRef);
+  return timeRef;
+}
+
+
+
+
+spooky.on('eval', function(html){
+  console.log("hello")
+  var table = html[0];
+  var tableRef = getTableData(table)
+  console.log("hello2")
+  // debugger
+  var script = html[1];
+  var unwanted  = /disableSelection.+;|paintCanDos\(\);|mySlider\(\);/g // Contains inaccessible functions
+  script = script.replace(unwanted, " ")
+
+  eval(script);
+  var applicants = {};
+  // for (var i in respondents) {
+  //   var responder = respondents[i];
+  //   applicants[responder.name] = responder.myCanDos
+  //   // Add all mycandos to a set
+  //   // Then find results using regex?
+  // }
+  console.log(applicants)
+  //TODO reconcile candos with actual times
+
+
+
   // console.log(respondents)
 })
 
 spooky.on('hello', function (greeting) {
-    console.log(greeting);
+  console.log(greeting);
 });
 
 spooky.on('log', function (log) {
-    if (log.space === 'remote') {
-        console.log(log.message.replace(/ \- .*/, ''));
-    }
+  if (log.space === 'remote') {
+    console.log(log.message.replace(/ \- .*/, ''));
+  }
 });
